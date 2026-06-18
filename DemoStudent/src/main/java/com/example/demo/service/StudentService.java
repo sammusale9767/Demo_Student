e7b99ee40ca961175b7c7f475c956e82b883b90a
DELL <DELL@DESKTOP-TH10H1Q>
2026-06-18 12:02:22 +0530

    First Commit

---
+++ DemoStudent/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Student;
+
+public interface StudentService {
+	
+	void add(Student s);
+//	List<Student> display();
+	
+	List<Student> display();
+}
