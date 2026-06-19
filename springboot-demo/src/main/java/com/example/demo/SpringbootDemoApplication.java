e41d6e1d555ed01ad93588e61bda29b33e4983b9
DELL <DELL@DESKTOP-TH10H1Q>
2026-06-19 11:37:17 +0530

    Second Change

---
+++ springboot-demo/src/main/java/com/example/demo/SpringbootDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+		System.out.println("Hello World");
+		
+		System.out.println("Here is Sarvesh. He is working on SpringBoot");
+	}
+
+}
